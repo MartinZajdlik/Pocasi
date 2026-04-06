bcf8ee948c3dd28199c39a6fbbce0f2243c66c1e
MartinZajdlik <m.zajdlik@seznam.cz>
2026-04-06 21:41:19 +0200

    update token

--- Pocasi/ContentView.swift
+++ Pocasi/ContentView.swift
@@ -20,18 +20,32 @@ struct ContentView: View {
     let iconSize: CGFloat = 30
     
     var body: some View {
-        Map(coordinateRegion: $mapView, annotationItems: lokality) {lokalita in
-            MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: lokalita.latitude, longitude: lokalita.longitude)){
-                VStack {
-                    Image(systemName: "mappin.cicrcle.fill")
-                        .resizable()
-                        .frame(width: iconSize, height: iconSize)
+        NavigationStack{
+            
+            Map(coordinateRegion: $mapView, annotationItems: lokality) {lokalita in
+                MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: lokalita.latitude, longitude: lokalita.longitude)){
+                    NavigationLink{
+                        DetailView(lokalita: lokalita)
+                    } label:{
+                            VStack {
+                                Image(systemName: "mappin.circle.fill")
+                                    .resizable()
+                                    .frame(width: iconSize, height: iconSize)
+                                    .foregroundColor(.red)
+                                
+                                Text(lokalita.name)
+                                    .foregroundColor(.primary)
+                                    .font(.caption)
+                            }
+                        }
+                    }
+                    
                 }
-                
             }
-            }
+            .ignoresSafeArea()
         }
     }
+    
 
 
 
---
+++ Pocasi/DetailView.swift
@@ -0,0 +1,72 @@
+//
+//  DetailView.swift
+//  Pocasi
+//
+//  Created by Martin Žajdlík on 06.04.2026.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let lokalita: Lokality
+    
+    var body: some View {
+        
+        VStack {
+            Text(lokalita.name)
+            Text("\(lokalita.latitude), \(lokalita.longitude)")
+                .onAppear{
+                    stahniData(lat: lokalita.latitude, lon: lokalita.longitude)
+                }
+                    
+                }
+        }
+    }
+    
+    func stahniData(lat:Double, lon: Double) {
+        
+        let urlString = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/\(lat),\(lon)?unitGroup=metric&include=days%2Ccurrent&key=\(APIKey.topSecret)&contentType=json"
+     
+        let url = URL(string: urlString)!
+        
+        let task = URLSession.shared.dataTask(with: url) {data, response, error in
+            
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else {
+                print("Missing data!")
+                return
+            }
+            
+            if let json = try? JSONSerialization.jsonObject(with: data){
+                print(json)
+                
+            }
+            
+            
+        }
+        task.resume()
+        
+    }
+    
+
+
+
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        let bs = Lokality(name: "Banska", latitude: 48.4587,longitude: 18.8931)
+        DetailView(lokalita: bs)
+    }
+}
+
+
+/*
+ 
+ https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Ostro%C5%BEsk%C3%A1%20Lhota?unitGroup=metric&include=days%2Ccurrent&key=UMMRCBHL5E9736JQRB3Q2UA7G&contentType=json
+ 
+ */
