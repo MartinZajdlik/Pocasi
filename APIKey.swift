41f27c75a71b8968740de14f8a4586aa6dc257c6
MartinZajdlik <m.zajdlik@seznam.cz>
2026-04-06 21:31:13 +0200

    connect to api URL

---
+++ APIKey.swift
@@ -0,0 +1,8 @@
+//
+//  APIKey.swift
+//  Pocasi
+//
+//  Created by Martin Žajdlík on 06.04.2026.
+//
+
+import Foundation
---
+++ DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Pocasi
+//
+//  Created by Martin Žajdlík on 06.04.2026.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
